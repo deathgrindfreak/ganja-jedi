6d3c1fbd5edc6beec612ea0a45eebd4135a284e6
Cooper Bell <john.cooper.bell@gmail.com>
2015-07-11 20:29:58 -0500

    Added basic message to Chris

--- project.clj
+++ project.clj
@@ -1,6 +1,14 @@
-(defproject ganja-jedi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+(defproject ganja-jedi "0.0.1"
+  :description "The official site for clan Ganja Jedi"
+  :url "http://ganjajedi.com"
+  :license {:name "Artistic License 2.0"
+            :url "http://opensource.org/licenses/Artistic-2.0"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [environ "0.5.0"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "ganjajedi.jar"
+  :profiles {:production {:env {:production true}}})
--- src/ganja_jedi/core.clj
+++
@@ -1,6 +0,0 @@
-(ns ganja-jedi.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
