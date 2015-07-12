5f55f20bf5bfca26257c270a161fcfa34501ac8c
Cooper Bell <john.cooper.bell@gmail.com>
2015-07-12 13:41:27 -0500

    Added bootstrap, messed with project.clj

--- project.clj
+++ project.clj
@@ -4,11 +4,17 @@
   :license {:name "Artistic License 2.0"
             :url "http://opensource.org/licenses/Artistic-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
+                 [compojure "1.3.1"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-defaults "0.1.2"]
                  [environ "0.5.0"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :plugins [[lein-ring "0.8.13"]
+            [environ/environ.lein "0.2.1"]]
+  :ring {:handler ganja-jedi.ganja/app}
   :hooks [environ.leiningen.hooks]
   :uberjar-name "ganjajedi.jar"
-  :profiles {:production {:env {:production true}}})
+  :profiles {:production {:env {:production true}}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}})
--- src/ganja_jedi/ganja.clj
+++ src/ganja_jedi/ganja.clj
@@ -1,9 +1,10 @@
 (ns ganja-jedi.ganja
-  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
-            [compojure.handler :refer [site]]
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [environ.core :refer [env]]))
 
 (defn splash []
@@ -11,15 +12,18 @@
    :headers {"Content-Type" "text/plain"}
    :body "Chris!!!!!!!!!!!"})
 
-(defroutes app
+(defroutes app-routes
   (GET "/" []
        (splash))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
-(defn -main [& [port]]
-  (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (site #'app) {:port port :join? false})))
+(def app
+  (wrap-defaults app-routes site-defaults))
+
+;; (defn -main [& [port]]
+;;   (let [port (Integer. (or port (env :port) 6666))]
+;;     (jetty/run-jetty (handler/site #'app) {:port port :join? false})))
 
 ;; For interactive development:
 ;; (.stop server)
